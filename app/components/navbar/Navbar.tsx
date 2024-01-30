bd99e101781ced054e8ec84446cee42c182afbfa
Taha S. ASAN <tahaasann@gmail.com>
2024-01-30 15:28:04 +0300

    airbnb-clone v0.0.3: container interface created

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,7 +1,21 @@
+import Container from "../container";
+
+
 const Navbar = () => {
-    return(
-        <div>Navbar!</div>
-    )
+    return (
+        <div className="fixed w-full bg-white z-10 shadow-sm">
+            <div
+                className="
+                py-4
+                border-b-[1px]
+                "
+            >
+                <Container>
+                    anything
+                </Container>
+            </div>
+        </div>
+    );
 }
 
 export default Navbar;
